46bbe4fff3114b8fdcd496ba2c545ff6a8269a11
李 <annielee197988@gmail.com>
2024-11-23 16:37:59 +0800

    裝飾品詳細資料

--- app/src/main/java/com/example/myapplication/ShopBuy.kt
+++ app/src/main/java/com/example/myapplication/ShopBuy.kt
@@ -1,20 +1,21 @@
 package com.example.myapplication
 
 import android.app.AlertDialog
-import android.content.Context
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.content.Context
 
 class ShopBuy {
     companion object {
+        // Show product dialog with price
         fun showProductDialog(
-            context: Context,
+            context: Context, // 改為 Context
             productName: String,
             productDescription: String,
             productImageRes: Int,
-            productPrice: Double // 新增金額參數
+            productPrice: Int // 新增金額參數
         ) {
             val builder = AlertDialog.Builder(context)
             val inflater = LayoutInflater.from(context)
@@ -43,6 +44,34 @@ class ShopBuy {
 
             dialog.show()
         }
+
+        // Show product end dialog without price
+        fun showProductEnd(
+            context: Context, // 改為 Context
+            productName: String,
+            productDescription: String,
+            productImageRes: Int,
+            productPrice: Int
+        ) {
+            val builder = AlertDialog.Builder(context)
+            val inflater = LayoutInflater.from(context)
+            val dialogLayout = inflater.inflate(R.layout.activity_shop_end, null)
+
+            builder.setView(dialogLayout)
+
+            val imageView = dialogLayout.findViewById<ImageView>(R.id.product_image)
+            val nameView = dialogLayout.findViewById<TextView>(R.id.product_name)
+            val descriptionView = dialogLayout.findViewById<TextView>(R.id.product_description)
+            val priceView = dialogLayout.findViewById<TextView>(R.id.product_price) // 用來顯示金額的 TextView
+
+            imageView.setImageResource(productImageRes)
+            nameView.text = productName
+            descriptionView.text = productDescription
+            priceView.text = "NT$ ${productPrice}"
+
+            val dialog = builder.create()
+
+            dialog.show()
+        }
     }
 }
-
